df5da81c768ca5f0f0cba184d9c4fbe0c8c61677
Luis Fontes <luisfontes@protonmail.com>
2026-03-14 22:16:19 +0000

    Add PackageJson Analyzer

---
+++ src/analyzers/packageJson.ts
@@ -0,0 +1,39 @@
+import * as jsonc from 'jsonc-parser'
+import * as vscode from 'vscode'
+import { isDangerousCommand } from '../dangerousCommands'
+import { Finding, FindingType } from '../types'
+import { StaticAnalyzer } from './staticAnalyzer'
+
+export class PackageJsonAnalyzer extends StaticAnalyzer {
+    alertOnBackgroundEdited(): boolean {
+        return false
+    }
+
+    canScanFile(uri: vscode.Uri): boolean {
+        return uri.fsPath.endsWith('package.json')
+    }
+
+    async checkFile(uri: vscode.Uri, content?: Uint8Array<ArrayBufferLike>): Promise<Finding[]> {
+
+        const data = content ?? await vscode.workspace.fs.readFile(uri)
+        const json = jsonc.parse(data.toString()) as Record<string, unknown>
+        return this.checkPackageJson(json, uri)
+    }
+
+    checkPackageJson(json: Record<string, any>, uri: vscode.Uri): Finding[] {
+        const findings: Finding[] = []
+        if (!json.scripts?.preinstall) return findings
+
+        const priority = isDangerousCommand(json.scripts.preinstall) ? 'high' : 'medium'
+
+        findings.push({
+            type: FindingType.PreinstallScript,
+            name: `Detected preinstall script in package.json`,
+            detail: `A preinstall script was detected in package.json. Preinstall scripts are a known attack vector for supply chain attacks, as they run before any dependencies are installed and can execute arbitrary code. Review the preinstall script to ensure it is safe.`,
+            priority: priority,
+            file: vscode.workspace.asRelativePath(uri, false)
+        })
+
+        return findings
+    }
+}
